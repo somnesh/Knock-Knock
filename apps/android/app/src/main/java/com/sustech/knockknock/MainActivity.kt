36f56eead0eef57c2e2a675e5c3e3c40fa93d401
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-07-18 21:14:18 +0530

    Folder structure setup

---
+++ apps/android/app/src/main/java/com/sustech/knockknock/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sustech.knockknock
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sustech.knockknock.ui.theme.KnockKnockTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KnockKnockTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KnockKnockTheme {
+        Greeting("Android")
+    }
+}
---
+++ apps/backend/api-service/src/main/java/com/sustech/knock_knock_services/KnockKnockApplication.java
@@ -0,0 +1,13 @@
+package com.sustech.knock_knock_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockKnockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockKnockApplication.class, args);
+	}
+
+}
